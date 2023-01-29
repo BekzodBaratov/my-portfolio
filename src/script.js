05f372d0a9c6e657ce43eddcf8ef895c48df57c7
Bekzod <bekzodbaratov334@gmail.com>
2023-01-29 12:57:51 +0500

    portfolio

--- src/script.js
+++ src/script.js
@@ -7,15 +7,36 @@ $(document).ready(function () {
       $(this).css("height", navbarUl / 6);
     });
 
-  const jobs = ["web developer", "strong junior developer", "front end developer"];
+  const portfolioArr = [
+    { name: "All", active: true },
+    { name: "Web sites", active: false },
+    { name: "Games", active: false },
+  ];
+  const portfolioImageArr = [
+    { id: 1, name: "portfolio_1.webp", link: "/", tag: "website" },
+    { id: 2, name: "portfolio_2.webp", link: "/", tag: "website" },
+    { id: 3, name: "portfolio_3.webp", link: "/", tag: "website" },
+    { id: 4, name: "portfolio_4.webp", link: "/", tag: "game" },
+    { id: 5, name: "portfolio_5.webp", link: "/", tag: "website" },
+    { id: 6, name: "portfolio_6.webp", link: "/", tag: "website" },
+    { id: 7, name: "portfolio_7.webp", link: "/", tag: "website" },
+    { id: 8, name: "portfolio_8.webp", link: "/", tag: "website" },
+    { id: 9, name: "portfolio_9.webp", link: "/", tag: "website" },
+    { id: 10, name: "portfolio_10.webp", link: "/", tag: "website" },
+    { id: 11, name: "portfolio_game_1.png", link: "/", tag: "game" },
+    { id: 12, name: "portfolio_game_2.webp", link: "/", tag: "game" },
+    { id: 13, name: "portfolio_game_3.webp", link: "/", tag: "game" },
+    { id: 14, name: "portfolio_game_4.webp", link: "/", tag: "game" },
+    { id: 15, name: "portfolio_game_5.webp", link: "/", tag: "game" },
+    { id: 16, name: "portfolio_game_6.webp", link: "/", tag: "game" },
+  ];
+  const jobs = ["web developer", "freelancer", "front-end developer"];
   const length = jobs.join("").length;
   let i = 0;
   setInterval(() => {
     i++;
     if (jobs[0].length >= i) {
       $(".job-anima").text(jobs[0].slice(0, i));
-      if (jobs[0].length == i) {
-      }
     } else if (jobs[0].length < i && jobs[1].length + jobs[0].length >= i) {
       let j = i - jobs[0].length;
       $(".job-anima").text(jobs[1].slice(0, j));
@@ -27,6 +48,7 @@ $(document).ready(function () {
     }
   }, 300);
 
+  // filip
   $(".navbar-link-a").click(function () {
     const dataId = $(this).attr("data-id");
 
@@ -49,10 +71,15 @@ $(document).ready(function () {
     }, 125);
   });
 
+  // filip active
   $(".colorAnima .childClick").click(function () {
     $(this).parent().toggleClass("colorActive");
   });
 
+  appendFuncLi(".portfolio-nav", portfolioArr);
+  appendImageLi(".portfolioImages", shuffle(portfolioImageArr));
+
+  // change color
   $(".colorType").click(function () {
     color = $(this).attr("color");
 
@@ -61,9 +88,12 @@ $(document).ready(function () {
     });
     $(this).removeClass("ring-transparent").addClass("ring-blue-900");
     $(".changeAnimaColor").css("backgroundColor", color);
+    $("head").append(`<style>.changeAnimaColorBefore::before {background-color: ${color};}</style>`);
+    $("head").append(`<style>.changeAnimaColorBefore::after {background-color: ${color};}</style>`);
     $(".changeAnimaColorText").css("color", color);
     $(".social-linkS").attr("stroke", color);
     $(".social-linkF").attr("fill", color);
+    $(".portfolio-nav li.active").css({ color: color });
     $(".fa-gear").css("color", color);
     $(".changeAnimaColorHover").each(function () {
       if ($(this).children("p").hasClass("navActive-p")) $(this).css("backgroundColor", color);
@@ -78,11 +108,37 @@ $(document).ready(function () {
     });
   });
 
+  // tags colors before color changing
+  $("head").append(`<style>.changeAnimaColorBefore::before {background-color: ${color};}</style>`);
+  $("head").append(`<style>.changeAnimaColorBefore::after {background-color: ${color};}</style>`);
   $(".changeAnimaColor").css("backgroundColor", color);
   $(".changeAnimaColorText").css("color", color);
   $(".social-linkS").attr("stroke", color);
   $(".social-linkF").attr("fill", color);
   $(".fa-gear").css("color", color);
+  $(".portfolio-nav li").click(function () {
+    for (let i = 0; i < portfolioArr.length; i++) {
+      if ($(this).attr("key") == i) {
+        $(this).attr("class", "active").css({ color: color });
+      } else {
+        $(this).siblings().attr("class", "").css({ color: "black" });
+      }
+    }
+    for (let i = 0; i < portfolioArr.length; i++) {
+      if ($(this).attr("key") == 0) {
+        appendImageLi(
+          ".portfolioImages",
+          shuffle(portfolioImageArr.filter((val) => val.tag == "game" || val.tag == "website"))
+        );
+      } else if ($(this).attr("key") == 1) {
+        appendImageLi(".portfolioImages", shuffle(portfolioImageArr.filter((val) => val.tag == "website")));
+      } else {
+        appendImageLi(".portfolioImages", shuffle(portfolioImageArr.filter((val) => val.tag == "game")));
+      }
+    }
+    // if($(this).attr('key')==i){}
+  });
+  $(".portfolio-nav li.active").css({ color: color });
   $(".changeAnimaColorHover").hover(
     function () {
       $(this).css("backgroundColor", color);
@@ -94,3 +150,34 @@ $(document).ready(function () {
     }
   );
 });
+
+function appendFuncLi(tag, val) {
+  let html = "";
+  for (var i = 0; i < val.length; i++) {
+    html += `<li key="${i}" class="${val[i].active ? "active" : ""}">${val[i].name}</li>`;
+  }
+  $(tag).append(html);
+}
+function appendImageLi(tag, val) {
+  let html = "";
+  for (let i = 0; i < val.length; i++) {
+    html += `
+    <li key="${val[i].id}" class="list-none overflow-hidden">
+      <a href="${val[i].link}">
+        <image src='./src/assets/portfolio/${val[i].name}' class="portfolioImageItem" alt="${val[i].tag}_image" />
+      </a>
+    </li>`;
+  }
+  $(tag).html(html);
+}
+function shuffle(array) {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+  }
+  return array;
+}
